c02894edbe4d57e7ba404e022b1f8d78c10a466b
Stephan Richter <github@srsoftware.de>
2013-05-05 23:01:56 +0200

    worked in bday implementation

--- src/Birthday.java
+++ src/Birthday.java
@@ -1,11 +1,33 @@
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
-
 public class Birthday {
 
+	private String time;
+	private String birtdate;
+
 	public Birthday(String bday) {
-		System.err.println(bday);
-		throw new NotImplementedException();
+		if (bday.startsWith(";VALUE=DATE-TIME:")) {
+			bday = bday.substring(17);
+		}
+		String year = bday.substring(0, 3);
+		bday = bday.substring(4);
+		if (bday.startsWith("-")) bday = bday.substring(1);
+		String month = bday.substring(0, 1);
+		bday = bday.substring(2);
+		if (bday.startsWith("-")) bday = bday.substring(1);
+		String day = bday.substring(0, 1);
+		bday = bday.substring(2);
+
+		birtdate = year + month + day;
+
+		if (!bday.isEmpty()) {
+			if (bday.startsWith("T")) {
+				time = bday;
+			} else {
+				System.err.println(bday);
+				throw new NotImplementedException();
+			}
+		}
 	}
 
 }
