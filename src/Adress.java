b772246aea52a5a9777e643ce4987adb8f68970a
Stephan Richter <github@srsoftware.de>
2013-05-05 13:40:47 +0200

    worked on adress resolution

--- src/Adress.java
+++ src/Adress.java
@@ -9,6 +9,9 @@ public class Adress {
 	
 	private boolean home=false;
 	private boolean work=false;
+	private String street;
+	private String city;
+	private String zip;
 
 	public Adress(String line) throws UnknownObjectException, InvalidFormatException {
 		if (!line.startsWith("ADR;")) throw new InvalidFormatException("Adress does not start with \"ADR;\"");
@@ -35,6 +38,31 @@ public class Adress {
 
 	private void readAddr(String line) {
 		if (line.equals(";;;;;;")) return;
-		throw new NotImplementedException();
+		String[] parts = line.split(";");
+		if (!parts[0].isEmpty()) {
+			System.err.println("found "+parts[0]+" @0 in "+line);
+			throw new NotImplementedException();
+		}
+		if (!parts[1].isEmpty()) {
+			System.err.println("found "+parts[1]+" @1 in "+line);
+			throw new NotImplementedException();
+		}
+		if (!parts[2].isEmpty()) {
+			street=parts[2];
+		}
+		if (!parts[3].isEmpty()) {
+			city=parts[3];
+		}
+		if (!parts[4].isEmpty()) {
+			System.err.println("found "+parts[4]+" @4 in "+line);
+			throw new NotImplementedException();
+		}
+		if (!parts[5].isEmpty()) {
+			zip=parts[5];
+		}
+		if (parts.length>6){
+			System.err.println("found more than 6 parts in "+line);
+			throw new NotImplementedException();
+		}
 	}
 }
