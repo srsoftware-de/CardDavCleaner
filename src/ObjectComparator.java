0c72a2acc0f0deb50919c2b79c06734ec85fc042
Stephan Richter <github@srsoftware.de>
2013-05-05 12:11:23 +0200

    added ObjectComparator for collections using treeSet

---
+++ src/ObjectComparator.java
@@ -0,0 +1,16 @@
+import java.util.Comparator;
+
+
+public class ObjectComparator implements Comparator<Object>{
+	private static ObjectComparator staticComparator;
+	
+	public int compare(Object t1,Object t2){
+		//System.out.println("Compared "+t1.getClass().getName()+" ("+t1.toString()+")\nwith "+t2.getClass().getName()+" ("+t2.toString()+").");
+		return t1.toString().compareTo(t2.toString());
+	}
+
+	public static ObjectComparator get() {
+	  if (staticComparator==null) staticComparator=new ObjectComparator();
+	  return staticComparator;
+  }
+}
