3f2488f00bddd6457f54c78c7c36e835a29c9132
Stephan Richter <github@srsoftware.de>
2013-12-26 22:50:59 +0100

    Name module updated to allow for usage of middle name

--- src/Name.java
+++ src/Name.java
@@ -6,25 +6,28 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class Name {	
 	
-	private String last;
+	private String family;
 	private String first;
-	private String title;
+	private String prefix;
+	private String middle;
 	
 	@Override
 	public String toString() {
 		StringBuffer sb=new StringBuffer();
 		sb.append("N:");
-		if (last!=null) sb.append(last);
+		if (family!=null) sb.append(family);
 		sb.append(';');
 		if (first!=null) sb.append(first);
-		sb.append(";;");
-		if (title!=null) sb.append(title);
+		sb.append(';');
+		if (middle!=null) sb.append(middle);
+		sb.append(";");
+		if (prefix!=null) sb.append(prefix);
 		sb.append(";");
 		return sb.toString();
 	}
 	
 	public String full(){
-		return title+" "+first+" "+last;
+		return prefix+" "+first+" "+middle+" "+family;
 	}
 	
 	public Name(String line) throws UnknownObjectException, InvalidFormatException {		
@@ -32,66 +35,74 @@ public class Name {
 		line=line.substring(2).trim();
 		if (line.contains(";")){
 			String[] parts = line.split(";");
-			if (parts.length>0) setLast(parts[0].trim());
+			if (parts.length>0) setFamily(parts[0].trim());
 			if (parts.length>1) setFirst(parts[1].trim());
-			if (parts.length>2 && !parts[2].isEmpty()) {
-				System.err.println("3rd part not implemented, yet:");
-				System.err.println(line);
-				for (String p:parts){
-					System.err.println(p);
-				}
-				throw new NotImplementedException();
-			}
-			if (parts.length>3) setTitle(parts[3].trim());
+			if (parts.length>2) setMiddle(parts[2].trim());
+			if (parts.length>3) setPrefix(parts[3].trim());
 			if (parts.length>4){
-				System.err.println("Name with more than two parts found:");
+				System.err.println("Name with more than 4 parts found:");
 				System.err.println(line);
 				for (String p:parts){
 					System.err.println(p);
 				}
 				throw new NotImplementedException();
 			}
-		} else last=line.substring(2); 
+		} else family=line.substring(2); 
 		
 	}
 	
-	private void setTitle(String string) {
+	private void setPrefix(String string) {
 		if (string.isEmpty()) return;
-		title=string;
+		prefix=string;
 	}
 
-	private void setLast(String string) {
+	private void setFamily(String string) {
 		if (string.isEmpty()) return;
-		last=string;		
+		family=string;		
 	}
 	private void setFirst(String string) {
 		if (string.isEmpty()) return;
 		first=string;		
 	}
+	private void setMiddle(String string) {
+		if (string.isEmpty()) return;
+		middle=string;		
+	}
 	
 	public boolean isEmpty() {
-		return ((last==null) && (first==null));	
+		return ((family==null) && (first==null));	
 	}
 	
 	public boolean equals(Name name){		
 		if (first!=null){
 			if (name.first==null) return false;
 			if (!name.first.equals(first)) return false;
-		} else if (name.first!=null) return false;		
+		} else if (name.first!=null) return false;
 		
-		if (last!=null){
-			if (name.last==null) return false;
-			if (!name.last.equals(last)) return false;
-		} else if (name.last!=null) return false;
+		if (middle!=null){
+			if (name.middle==null) return false;
+			if (!name.middle.equals(middle)) return false;
+		} else if (name.middle!=null) return false;
+
+		if (family!=null){
+			if (name.family==null) return false;
+			if (!name.family.equals(family)) return false;
+		} else if (name.family!=null) return false;
+		
+		if (prefix!=null){
+			if (name.prefix==null) return false;
+			if (!name.prefix.equals(prefix)) return false;
+		} else if (name.prefix!=null) return false;
+
 		return true;
 	}
 
 	public String title(){
-		return title;
+		return prefix;
 	}
 
 	public String last() {
-		return last;
+		return family;
 	}
 
 	public String first() {
@@ -105,7 +116,8 @@ public class Name {
 	public String canonical() {
 		TreeSet<String> parts=new TreeSet<String>(ObjectComparator.get());
 		if (first!=null) parts.add(ascii(first).toLowerCase());
-		if (last!=null) parts.add(ascii(last).toLowerCase());	
+		if (middle!=null) parts.add(ascii(middle).toLowerCase());
+		if (family!=null) parts.add(ascii(family).toLowerCase());	
 		
 		return parts.toString().replace("[", "").replace("]", ""); // sorted set of name parts
 	}
