cf0b78b4c0c0f9b6e12b2ddeb49fca312380d085
Stephan Richter <github@srsoftware.de>
2014-01-23 20:33:56 +0100

    added test Method

--- src/MD5Hash.java
+++ src/MD5Hash.java
@@ -10,9 +10,20 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5Hash implements Comparable<MD5Hash>{
 	public static void test() {
-		// TODO Auto-generated method stub
-		
+		String text="Dies ist ein Test";
+		System.out.print("MD5 hashing...");
+		try {
+			MD5Hash hash = new MD5Hash(text);
+			if (hash.toString().equals("6cddeb6a2f0582c82dee9a38e3f035d7")){
+				System.out.println("ok.");
+			} else {
+				System.err.println("failed: "+hash);
+			}
+		} catch (NoSuchAlgorithmException e) {
+			System.err.println("failed!");
+		}		
 	}
+	
 	String hashtext;
 	/**
 	 * create new hash value for given object
