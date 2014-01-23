379445683cf141f68663ec6a66d336fb7bc156f8
Stephan Richter <srichter@srsoftware.de>
2014-01-23 16:24:09 +0100

    implemented merge methods

--- src/Url.java
+++ src/Url.java
@@ -7,7 +7,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
 
-public class Url implements ChangeListener, Comparable<Url> {
+public class Url extends Mergable<Url> implements ChangeListener, Comparable<Url> {
 	
 	private boolean home=false;
 	private boolean work=false;
@@ -18,30 +18,6 @@ public class Url implements ChangeListener, Comparable<Url> {
 	private JCheckBox workBox;
 	private VerticalPanel form;
 	private Pattern ptr = Pattern.compile("^(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]");
-
-	public VerticalPanel editForm() {
-		form=new VerticalPanel("Web Adress");
-		if (invalid) form.setBackground(Color.red);
-		if (isEmpty()) form.setBackground(Color.yellow);
-		form.add(urlField=new InputField("URL",url));
-		urlField.addEditListener(this);
-		form.add(homeBox=new JCheckBox("Home",home));
-		homeBox.addChangeListener(this);
-		form.add(workBox=new JCheckBox("Work",work));
-		workBox.addChangeListener(this);
-		form.scale();
-		return form;
-	}
-	
-	public String toString() {
-		StringBuffer sb=new StringBuffer();
-		sb.append("URL");
-		if (home) sb.append(";TYPE=HOME");
-		if (work) sb.append(";TYPE=WORK");
-		sb.append(":");
-		sb.append(url);
-		return sb.toString();
-	}
 
 	public Url(String content) throws UnknownObjectException, InvalidFormatException {
 		if (!content.startsWith("URL")) throw new InvalidFormatException("Url does not start with \"URL\"");
@@ -70,6 +46,62 @@ public class Url implements ChangeListener, Comparable<Url> {
 		}
 		readUrl(line.substring(1));		
 	}
+	
+	public int compareTo(Url otherUrl) {
+		return this.toString().compareTo(otherUrl.toString());
+	}
+
+	public VerticalPanel editForm() {
+		form=new VerticalPanel("Web Adress");
+		if (invalid) form.setBackground(Color.red);
+		if (isEmpty()) form.setBackground(Color.yellow);
+		form.add(urlField=new InputField("URL",url));
+		urlField.addEditListener(this);
+		form.add(homeBox=new JCheckBox("Home",home));
+		homeBox.addChangeListener(this);
+		form.add(workBox=new JCheckBox("Work",work));
+		workBox.addChangeListener(this);
+		form.scale();
+		return form;
+	}
+
+	@Override
+  public boolean isCompatibleWith(Url other) {
+		if (different(url, other.url)) return false;
+	  return true;
+  }
+
+	public boolean isEmpty() {
+		return url==null || url.isEmpty();
+	}
+
+	@Override
+  public boolean mergeWith(Url other) {
+		url=merge(url, other.url);
+		if (other.home) home=true;
+		if (other.work) work=true;
+	  return true;
+  }
+
+	public void stateChanged(ChangeEvent arg0) {
+		update();
+	}
+	
+	public String toString() {
+		StringBuffer sb=new StringBuffer();
+		sb.append("URL");
+		if (home) sb.append(";TYPE=HOME");
+		if (work) sb.append(";TYPE=WORK");
+		sb.append(":");
+		sb.append(url);
+		return sb.toString();
+	}
+
+	private void checkValidity() {
+		invalid=false;
+		if (url==null) return;
+		invalid=!ptr.matcher(url).matches();
+	}
 
 	private void readUrl(String line) {
 		line=line.trim();
@@ -80,20 +112,6 @@ public class Url implements ChangeListener, Comparable<Url> {
 		checkValidity();
 	}
 
-	private void checkValidity() {
-		invalid=false;
-		if (url==null) return;
-		invalid=!ptr.matcher(url).matches();
-	}
-
-	public boolean isEmpty() {
-		return url==null || url.isEmpty();
-	}
-
-	public void stateChanged(ChangeEvent arg0) {
-		update();
-	}
-	
 	private void update(){
 		readUrl(urlField.getText());
 		home=homeBox.isSelected();
@@ -103,10 +121,6 @@ public class Url implements ChangeListener, Comparable<Url> {
 		} else {
 			form.setBackground(invalid?Color.red:Color.green);
 		}	
-	}
-
-	public int compareTo(Url otherUrl) {
-		return this.toString().compareTo(otherUrl.toString());
 	}
 
 }
