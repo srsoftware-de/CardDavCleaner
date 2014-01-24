4006ea1107459e499ef9152873fb882915920172
Stephan Richter <srichter@srsoftware.de>
2014-01-24 15:12:15 +0100

    implemented some tests for Messenger

--- src/Messenger.java
+++ src/Messenger.java
@@ -7,7 +7,283 @@ import javax.swing.event.ChangeListener;
 
 
 public class Messenger extends Mergable<Messenger> implements ChangeListener, Comparable<Messenger> {
-	
+	public static void test() {
+		try {
+			System.out.print("Messenger creation test (null)...");
+			String testCase = null;
+			try {
+				Messenger nM = new Messenger(testCase);
+				System.err.println("failed: " + nM);
+				System.exit(-1);
+			} catch (InvalidFormatException e) {
+				System.out.println("ok");
+      }
+
+			System.out.print("Messenger creation test (empty)...");
+			testCase = "IMPP:";
+			try {
+				Messenger eM = new Messenger(testCase);
+				System.err.println("failed: " + eM);
+				System.exit(-1);
+			} catch (UnknownObjectException e){
+				System.out.println("ok");
+			}
+
+			System.out.print("Messenger creation test (empty ICQ)...");
+			testCase = "IMPP:icq:";
+			Messenger eICQ = new Messenger(testCase);
+			if (eICQ.toString().equals(testCase) && !eICQ.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + eICQ);
+				System.exit(-1);
+			}
+
+			System.out.print("Messenger creation test (empty Skype)...");
+			testCase = "IMPP:skype:";
+			Messenger eSkype = new Messenger(testCase);
+			if (eSkype.toString().equals(testCase) && !eSkype.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + eSkype);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (empty MSN)...");
+			testCase = "IMPP:msn:";
+			Messenger eMSN = new Messenger(testCase);
+			if (eMSN.toString().equals(testCase) && !eMSN.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + eMSN);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (empty AIM)...");
+			testCase = "IMPP:aim:";
+			Messenger eAIM = new Messenger(testCase);
+			if (eAIM.toString().equals(testCase) && !eAIM.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + eAIM);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (empty Facebook)...");
+			testCase = "IMPP:facebook:";
+			Messenger eFB = new Messenger(testCase);
+			if (eFB.toString().equals(testCase) && !eFB.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + eFB);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (ICQ)...");
+			testCase = "IMPP:icq:123456";
+			Messenger icq = new Messenger(testCase);
+			if (icq.toString().equals(testCase) && !icq.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + icq);
+				System.exit(-1);
+			}
+
+			System.out.print("Messenger creation test (Skype)...");
+			testCase = "IMPP:skype:test";
+			Messenger skype = new Messenger(testCase);
+			if (skype.toString().equals(testCase) && !skype.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + skype);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (MSN)...");
+			testCase = "IMPP:msn:test@example.com";
+			Messenger msn = new Messenger(testCase);
+			if (msn.toString().equals(testCase) && !msn.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + msn);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (AIM)...");
+			testCase = "IMPP:aim:test@example.com";
+			Messenger aim = new Messenger(testCase);
+			if (aim.toString().equals(testCase) && !aim.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + aim);
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger creation test (Facebook)...");
+			testCase = "IMPP:facebook:toast@example.com";
+			Messenger fb = new Messenger(testCase);
+			if (fb.toString().equals(testCase) && !fb.isInvalid()) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + fb);
+				System.exit(-1);
+			}
+			
+			Messenger[] messengers = { eICQ,eSkype,eMSN,eAIM,eFB,icq,skype,msn,aim,fb };
+
+			System.out.print("Messenger isEmpty test...");
+			int comp = 0;
+			int num = 0;
+			for (Messenger m : messengers) {
+				comp++;
+				if (!m.isEmpty()) {
+					num++;
+				} else if (m == eICQ) {
+					num++;
+				} else if (m == eSkype) {
+					num++;
+				} else if (m == eMSN) {
+					num++;
+				} else if (m == eAIM) {
+					num++;
+				} else if (m == eFB) {
+					num++;
+				}
+			}
+			if (num == comp) {
+				System.out.println("ok");
+			} else {
+				System.err.println(num + "/" + comp + " => failed");
+				System.exit(-1);
+			}
+
+			/*
+			System.out.print("Messenger compare test...");
+			comp = 0;
+			num = 0;
+			for (Messenger m : mails) {
+				comp++;
+				if (m.compareTo(netM) != 0 && m.compareTo(netM) == -netM.compareTo(m)) {
+					num++;
+				} else {
+					if (netM==m){
+						num++;
+					}
+				}
+			}
+			if (comp == num) {
+				System.out.println("ok");
+			} else {
+				System.err.println(num + "/" + comp + " => failed");
+				System.exit(-1);
+			}
+
+			System.out.print("Messenger compatibility test...");
+			comp = 0;
+			num = 0;
+			for (Messenger a : mails) {
+				for (Messenger b : mails) {
+					num++;
+					if (a.isCompatibleWith(b)) {
+						comp++;
+					} else {
+						String concat = (a + "" + b).replace("EMAIL", "").replace(";TYPE=INTERNET", "").replace(";TYPE=WORK", "").replace(";TYPE=HOME", "").replaceFirst(":", "");
+						if (concat.equals("net@example.com:test.test-24+a@test.example.com") ||
+								concat.equals("net@example.com:steinlaus") ||
+								concat.equals("net@example.com:work@example.com") ||
+								concat.equals("net@example.com:home@example.com") ||
+								concat.equals("steinlaus:home@example.com") ||
+								concat.equals("steinlaus:net@example.com") ||
+								concat.equals("steinlaus:test.test-24+a@test.example.com") ||
+								concat.equals("steinlaus:work@example.com") ||
+								concat.equals("test.test-24+a@test.example.com:home@example.com") ||
+								concat.equals("test.test-24+a@test.example.com:net@example.com") ||
+								concat.equals("test.test-24+a@test.example.com:steinlaus") ||
+								concat.equals("test.test-24+a@test.example.com:work@example.com") ||
+								concat.equals("work@example.com:home@example.com") ||
+								concat.equals("work@example.com:net@example.com") ||
+								concat.equals("work@example.com:steinlaus") ||
+								concat.equals("work@example.com:test.test-24+a@test.example.com") ||
+								concat.equals("home@example.com:steinlaus") ||
+								concat.equals("home@example.com:net@example.com") ||
+								concat.equals("home@example.com:test.test-24+a@test.example.com") ||
+								concat.equals("home@example.com:work@example.com")) {
+							comp++;
+						} else {
+							System.err.println(a + " <=> " + b);
+						}
+					}
+				}
+			}
+			if (comp == num) {
+				System.out.println("ok");
+			} else {
+				System.err.println(num + "/" + comp + " => failed");
+				System.exit(-1);
+			}
+			
+			System.out.print("Messenger clone test...");
+			comp=0;
+			num=0;
+			for (Messenger m:mails){
+				comp++;
+				try {
+					if (m.toString().equals(m.clone().toString())){
+						num++;
+					}
+				} catch (CloneNotSupportedException e) {
+				}
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {				
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+
+			System.out.print("Messenger merge test...");
+			comp=0;
+			num=0;
+			for (Messenger m:mails){
+				try {
+					comp+=2;
+					Messenger clone1=(Messenger) m.clone();
+					Messenger clone2=(Messenger) netM.clone();
+					
+					if (clone1.mergeWith(netM) && clone1.toString().equals(netM.toString())) num++;
+					if (clone2.mergeWith(m) && clone2.toString().equals(netM.toString())) num++;
+					if (comp>num){
+						if ((m.adress!=null && !m.adress.isEmpty()) && (netM.adress!=null && !netM.adress.isEmpty()) && !m.address().equals(netM.adress)){
+							num+=2;
+						}
+					}
+					if (comp>num){
+						System.out.println();
+						System.out.println("fb: "+netM);
+						System.out.println(" b: "+m);
+						System.out.println("merged:");
+						System.out.println("fb: "+clone2);
+						System.out.println(" b: "+clone1);
+					}
+				} catch (CloneNotSupportedException e) {
+					e.printStackTrace();
+				}				
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {				
+				System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			/**/
+		} catch (UnknownObjectException e) {
+      e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+
+		}
+
+	}
 	private boolean aim=false;
 	private boolean icq=false;
 	private boolean skype=false;
@@ -24,7 +300,7 @@ public class Messenger extends Mergable<Messenger> implements ChangeListener, Co
 	private VerticalPanel form;
 	
 	public Messenger(String content) throws InvalidFormatException, UnknownObjectException {
-		if (!content.startsWith("IMPP:")) throw new InvalidFormatException("Messenger adress does not start with \"IMPP:\"");
+		if (content==null ||!content.startsWith("IMPP:")) throw new InvalidFormatException("Messenger adress does not start with \"IMPP:\"");
 		String line = content.substring(5);
 		while(!line.startsWith(":")){
 			String upper = line.toUpperCase();
@@ -53,7 +329,7 @@ public class Messenger extends Mergable<Messenger> implements ChangeListener, Co
 				line=line.substring(8);
 				continue;
 			} 
-			throw new UnknownObjectException(line+" in "+content);
+			throw new UnknownObjectException("\""+line+"\" in "+content);
 		}
 		readAddr(line.substring(1));		
 	}
@@ -156,13 +432,13 @@ public class Messenger extends Mergable<Messenger> implements ChangeListener, Co
 		if (msn) sb.append("msn");
 		if (facebook) sb.append("facebook");
 		sb.append(":");
-		sb.append(nick);
+		if (nick!=null && !nick.isEmpty()) sb.append(nick);
 		return sb.toString();
 	}
 
 	private void readAddr(String line) {
 		while (line.startsWith(":")) line=line.substring(1);
-		if (line.isEmpty()) return;		
+		if (line.trim().isEmpty()) return;		
 		nick = line.toLowerCase();
 	}
 
