4c81bd86f917b1f53dbb51d7676ab1df0abd8f89
Stephan Richter <srichter@srsoftware.de>
2014-01-24 16:42:49 +0100

    completed Tests for Organizations

--- src/Organization.java
+++ src/Organization.java
@@ -6,7 +6,241 @@ import javax.swing.event.ChangeListener;
 
 
 public class Organization extends Mergable<Organization> implements ChangeListener, Comparable<Organization> {
-	
+	public static void test() {
+		try {
+			System.out.print("Organization creation (null)...");
+			try {
+				Organization nullO = new Organization(null);
+				System.err.println("failed: " + nullO);
+				System.exit(-1);
+			} catch (InvalidFormatException ife) {				
+				System.out.println("ok");
+			}
+			
+			System.out.print("Organization creation (empty)...");
+			String testcase="ORG:;;";
+			Organization emptyA = new Organization(testcase.replace(";",""));
+			Organization emptyB = new Organization(testcase);
+			if (emptyA.toString().equals(testcase) && emptyB.toString().equals(testcase)) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + emptyA);
+				System.exit(-1);
+			}
+
+			System.out.print("Organization creation (name)...");
+			testcase="ORG:eman;;";
+			Organization namA = new Organization(Tests.reversed(testcase));
+			Organization namB = new Organization(testcase);
+			if (namA.toString().equals(Tests.reversed(testcase)+";;")&&namB.toString().equals(testcase)) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + namA+"/"+namB);
+				System.exit(-1);
+			}
+
+			System.out.print("Organization creation (extended)...");
+			testcase="ORG:;dednetxe;";
+			Organization extA = new Organization(Tests.reversed(testcase));
+			Organization extB = new Organization(testcase);
+			if (extA.toString().equals(Tests.reversed(testcase)+";")&&extB.toString().equals(testcase)) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + extA+"/"+extB);
+				System.exit(-1);
+			}
+
+			System.out.print("Organization creation (complete)...");
+			testcase="ORG:eman;dednetxe;";
+			Organization fullA = new Organization(Tests.reversed(testcase));
+			Organization fullB = new Organization(testcase);
+			if (fullA.toString().equals(Tests.reversed(testcase)+";")&&fullB.toString().equals(testcase)) {
+				System.out.println("ok");
+			} else {
+				System.err.println("failed: " + fullA+"/"+fullB);
+				System.exit(-1);
+			}
+
+			Organization [] orgs1={emptyA,namA,extA,fullA};
+			Organization [] orgs2={emptyB,namB,extB,fullB};
+			
+			System.out.print("Organization isEmpty test...");
+			int comp=0;
+			int num=0;
+			for (Organization a:orgs1){
+				comp++;
+				if (!a.isEmpty()){
+					num++;
+				} else if (a==emptyA) {
+					num++;
+				}
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {
+				System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			
+			System.out.print("Organization compare test...");
+			comp=0;
+			num=0;
+			for (Organization a:orgs1){
+				num++;
+				if (a.compareTo(fullB)!=0 && a.compareTo(fullB)==-fullB.compareTo(a)){
+					comp++;
+				}
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {
+				System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+				
+			System.out.print("Organization compatibility test 1 (self)...");
+			comp=0;
+			num=0;
+			for (Organization a:orgs1){
+				for (Organization b:orgs1){
+					num++;
+					if (a.isCompatibleWith(b)) {
+						comp++;
+					} else {
+						System.err.println(a+" <=> "+b);
+					}
+				}
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+
+		
+			System.out.print("Organization compatibility test 2 (other)...");
+			comp=0;
+			num=0;
+			for (Organization a:orgs1){
+				for (Organization b:orgs2){
+					num++;
+					if (a.isCompatibleWith(b)) {
+						comp++;
+					} else {
+						String concat=(a+""+b).replace("ORG:","").replace(";", "");
+						if (concat.equals("nameeman") ||
+							  concat.equals("nameemandednetxe") ||
+							  concat.equals("extendeddednetxe") ||
+							  concat.equals("extendedemandednetxe") ||
+							  concat.equals("nameextendedeman") ||
+							  concat.equals("nameextendeddednetxe") ||
+							  concat.equals("nameextendedemandednetxe")){
+							comp++;
+						} else {
+							System.err.println(a+" <=> "+b);
+						}
+					}
+				}
+				
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			
+			System.out.print("Organization clone test...");
+			comp=0;
+			num=0;
+			for (Organization a:orgs1){
+				num++;
+				try {
+					if (a.toString().equals(a.clone().toString())){
+						comp++;
+					}
+				} catch (CloneNotSupportedException e) {
+				}
+			}
+			for (Organization b:orgs2){
+				num++;
+				try {
+					if (b.toString().equals(b.clone().toString())){
+						comp++;
+					}
+				} catch (CloneNotSupportedException e) {
+				}
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {				
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			
+			
+			System.out.print("Organization merge test 1 (compatible)...");
+			comp=0;
+			num=0;
+			for (Organization a:orgs1){
+				try {
+					comp+=2;
+					Organization clone1=(Organization) a.clone();
+					Organization clone2=(Organization) fullA.clone();
+					
+					if (clone1.mergeWith(fullA) && clone1.toString().equals(fullA.toString())) num++;
+					if (clone2.mergeWith(a) && clone2.toString().equals(fullA.toString())) num++;
+				
+				} catch (CloneNotSupportedException e) {
+					e.printStackTrace();
+				}				
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {				
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			
+			System.out.print("Organization merge test 2 (incompatible)...");
+			comp=0;
+			num=0;
+			for (Organization b:orgs2){
+				try {
+					comp+=2;
+					Organization clone1=(Organization) b.clone();
+					Organization clone2=(Organization) fullA.clone();
+					
+					if (!clone1.mergeWith(fullA)) {
+						num++;
+					} else if (b==emptyB){
+						num++;
+					}
+					if (!clone2.mergeWith(b)) {
+						num++;
+					} else if (b==emptyB){
+						num++;
+					}
+				
+				} catch (CloneNotSupportedException e) {
+					e.printStackTrace();
+				}				
+			}
+			if (comp==num){
+				System.out.println("ok");
+			} else {				
+								System.err.println(num+"/"+comp+" => failed");
+				System.exit(-1);
+			}
+			
+			// continue tests here
+		} catch (UnknownObjectException e) {
+			e.printStackTrace();
+		} catch (InvalidFormatException e) {
+			e.printStackTrace();
+		}
+	}	
 	String name=null;
 	String extended=null;
 	private InputField nameField;
@@ -14,7 +248,7 @@ public class Organization extends Mergable<Organization> implements ChangeListen
 	private VerticalPanel form;
 	
 	public Organization(String content) throws UnknownObjectException, InvalidFormatException {		
-		if (!content.startsWith("ORG:")) throw new InvalidFormatException("Organization does not start with \"ORG:\"");
+		if (content==null || !content.startsWith("ORG:")) throw new InvalidFormatException("Organization does not start with \"ORG:\"");
 		String line=content.substring(4);
 		if (line.contains(";")){
 			String[] parts = line.split(";",0);
@@ -50,6 +284,7 @@ public class Organization extends Mergable<Organization> implements ChangeListen
 
 	@Override
   public boolean mergeWith(Organization other) {
+		if (!isCompatibleWith(other))return false;
 		name=merge(name,other.name);
 		extended=merge(extended, other.extended);
 	  return true;
