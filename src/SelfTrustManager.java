809b6b9b52de06c32a789236cfd66027e02534b9
Stephan Richter <github@srsoftware.de>
2014-02-12 09:15:14 +0100

    implemented gui for showing certificate info

--- src/SelfTrustManager.java
+++ src/SelfTrustManager.java
@@ -1,5 +1,7 @@
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.text.SimpleDateFormat;
+
 import javax.net.ssl.X509TrustManager;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -11,11 +13,18 @@ import sun.security.validator.ValidatorException;
  **/
 public class SelfTrustManager implements X509TrustManager {
 
-	private static final int CN = 0;
 	private X509TrustManager externalTrustManager;
+	
+	private static String _(String text) { 
+		return Translations.get(text);
+	}
+
+	private static String _(String key, Object insert) {
+		return Translations.get(key, insert);
+	}
 
 	public SelfTrustManager(X509TrustManager externalTrustManager) {
-		if (externalTrustManager == null) throw new IllegalArgumentException("No X509-Trust Manager found.");
+		if (externalTrustManager == null) throw new IllegalArgumentException(_("No X509-Trust Manager found."));
 		this.externalTrustManager = externalTrustManager;
 	}
 
@@ -29,20 +38,11 @@ public class SelfTrustManager implements X509TrustManager {
 			System.out.print("checkClientTrusted(");
 			externalTrustManager.checkClientTrusted(certificates, authType);
 		} catch (ValidatorException ve) {
-			if (certificates == null || certificates.length == 0) throw new IllegalArgumentException("Empty certificate chain supplied!");
-			if (authType == null || authType.isEmpty()) throw new IllegalArgumentException("No authType given!");
+			if (certificates == null || certificates.length == 0) throw new IllegalArgumentException(_("Empty certificate chain supplied!"));
+			if (authType == null || authType.isEmpty()) throw new IllegalArgumentException(_("No authType given!"));
 			for (X509Certificate certificate : certificates) {
 				certificate.checkValidity();
-				System.out.println("ID:"+certificate.getIssuerUniqueID());
-				System.out.println("Principal:"+certificate.getIssuerX500Principal());
-				System.out.println("DN:"+certificate.getIssuerDN());
-				System.out.println("alt:"+certificate.getIssuerAlternativeNames());
-				System.out.println("from:"+certificate.getNotBefore());
-				System.out.println("to:"+certificate.getNotAfter());
-				System.out.println("serial:"+certificate.getSerialNumber());
-				System.out.println("version:"+certificate.getVersion());
-				System.out.println("=======");				
-				JOptionPane.showConfirmDialog(null, formatCert(certificate), getCertPart(certificate,CN), JOptionPane.YES_NO_OPTION);
+				JOptionPane.showConfirmDialog(null, formatCert(certificate), getCertPart(certificate,"CN"), JOptionPane.YES_NO_OPTION);
 			}
 			System.out.println("," + authType);
 
@@ -50,9 +50,14 @@ public class SelfTrustManager implements X509TrustManager {
 		}
 	}
 	
-	String getCertPart(X509Certificate certificate, int part){
-		System.out.println(certificate.getIssuerX500Principal().getName());
-		return "Cert";
+	String getCertPart(X509Certificate certificate, String key){
+		if (key==null) System.out.println(certificate.getIssuerX500Principal());
+		String[] parts = certificate.getIssuerX500Principal().toString().split(", ");
+		for (String part:parts){
+			String[] map = part.split("=");
+			if (map[0].equals(key)) return map[1];
+		}
+		return "";
 	}
 
 	/**
@@ -62,31 +67,35 @@ public class SelfTrustManager implements X509TrustManager {
 	 **/
 	public void checkServerTrusted(X509Certificate[] certificates, String authType) throws CertificateException {
 		try {
-			System.out.print("checkServerTrusted(");
 			externalTrustManager.checkServerTrusted(certificates, authType);
 		} catch (ValidatorException ve) {
 			if (certificates == null || certificates.length == 0) throw new IllegalArgumentException("Empty certificate chain supplied!");
 			if (authType == null || authType.isEmpty()) throw new IllegalArgumentException("No authType given!");
 			for (X509Certificate certificate : certificates) {
 				certificate.checkValidity();
-				System.out.println("ID:"+certificate.getIssuerUniqueID());
-				System.out.println("Principal:"+certificate.getIssuerX500Principal());
-				System.out.println("DN:"+certificate.getIssuerDN());
-				System.out.println("alt:"+certificate.getIssuerAlternativeNames());
-				System.out.println("from:"+certificate.getNotBefore());
-				System.out.println("to:"+certificate.getNotAfter());
-				System.out.println("serial:"+certificate.getSerialNumber());
-				System.out.println("version:"+certificate.getVersion());
-				System.out.println("=======");				
-				JOptionPane.showConfirmDialog(null, formatCert(certificate), getCertPart(certificate,CN), JOptionPane.YES_NO_OPTION);
+				JOptionPane.showConfirmDialog(null, formatCert(certificate), getCertPart(certificate,"CN"), JOptionPane.YES_NO_OPTION);
 			}			
 			System.exit(-1);
 		}
 	}
 
 	private VerticalPanel formatCert(X509Certificate certificate) {
-		VerticalPanel result=new VerticalPanel("Certificate Info");
-		result.add(new JLabel("Certificate Information here"));
+    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");           
+		VerticalPanel result=new VerticalPanel();
+		result.add(new JLabel(_("This website uses a certificate, which is not in your list of trusted certificates!")));
+		VerticalPanel info=new VerticalPanel(_("Certificate Information"));
+		info.add(new JLabel(_("Common Name:")+" "+getCertPart(certificate, "CN")));
+		info.add(new JLabel(_("Organization:")+" "+getCertPart(certificate, "O")));
+		info.add(new JLabel(_("Organizational Unit:")+" "+getCertPart(certificate, "OU")));
+		info.add(new JLabel(_("Location:")+" "+getCertPart(certificate, "L")));
+		info.add(new JLabel(_("State:")+" "+getCertPart(certificate, "ST")));
+		info.add(new JLabel(_("Country:")+" "+getCertPart(certificate, "C")));
+		info.add(new JLabel(_("Email:")+" "+getCertPart(certificate, "EMAILADDRESS")));
+		String sn=certificate.getSerialNumber().toString(16).toUpperCase().replaceAll("(.{2})(?!$)", "$1:"); // hex form
+		info.add(new JLabel(_("Serial Number:")+" "+sn));
+		info.add(new JLabel(_("Validity period:")+" "+df.format(certificate.getNotBefore())+" - "+df.format(certificate.getNotAfter())));
+		info.scale();
+		result.add(info);
 		result.scale();
 		return result;
 	}
