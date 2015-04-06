be644e6c4ef751e789d55eabfbbe2b62da08aa9d
Stephan Richter <github@srsoftware.de>
2015-04-06 13:34:23 +0200

    altered client class for more flexibility

---
+++ src/Client.java
@@ -0,0 +1,31 @@
+import java.util.TreeMap;
+
+
+public class Client {
+	TreeMap<String, Integer> maxAddresses, maxPhones, maxMails, maxUrls,maxMessengers;
+	int maxOrgs;
+	int maxNicknames;
+	int maxLabels;
+	public String name;
+
+	public Client(String name,
+								TreeMap<String,Integer> addresses,
+								TreeMap<String,Integer> phones,
+								TreeMap<String,Integer> mails,
+								TreeMap<String,Integer> urls,
+								TreeMap<String,Integer> messengers,
+								Integer orgs,
+								Integer nicknames,
+								Integer categories,
+								Integer labels) {
+		this.name=name;
+		maxAddresses=addresses;
+		maxPhones=phones;
+		maxMails=mails;
+		maxUrls=urls;
+		maxOrgs=(orgs==null)?0:orgs;
+		maxMessengers=messengers;
+		maxNicknames=nicknames;
+		maxLabels=labels;
+	}	
+}
